256a7376830f7c0b094da7a20ae8528a4d8b4624
Lucas LJSM. Jose Serra Matthes <l.j.s.matths@gmail.com>
2021-03-15 15:23:04 -0300

    aula 4

--- src/componentes/ChallengeBox.tsx
+++ src/componentes/ChallengeBox.tsx
@@ -1,29 +1,29 @@
 import { useContext } from 'react';
 import { ChallengesContext } from '../contexts/ChallengesContext';
+
 import style from '../style/componentes/ChallengeBox.module.css';
 
 export function ChallengeBox() {
 
-    const contextData = useContext(ChallengesContext);
-
-    const hasActiveChallenge = true;
+    const {activeChallenge, resetChallenge} = useContext(ChallengesContext);
 
     return (
         <div className={style.challengeBoxContainer}>
-            { hasActiveChallenge ? (
+            { activeChallenge ? (
                 <div className={style.challengeActive}>
-                    <header>Ganhe 400xp</header>
+                    <header>Ganhe {activeChallenge.amount} xp</header>
 
                     <main>
-                        <img src="icons/body.svg" />
+                        <img src={`icons/${activeChallenge.type}.svg`} />
                         <strong>Novo desafio</strong>
-                        <p>Levante e faça uma caminhada de 3 minutos.</p>
+                        <p>{activeChallenge.description}</p>
                     </main>
 
                     <footer>
                         <button 
                         type='button'
                         className={style.challengeFailedButton} 
+                        onClick={resetChallenge}
                         >
                             Falhei
                         </button>
--- src/componentes/ExperienceBar.tsx
+++ src/componentes/ExperienceBar.tsx
@@ -1,14 +1,23 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../style/componentes/ExperienceBar.module.css';
 
+
+
 export function ExperienceBar() {
+
+    const { currentExperience, experienceToNextLevel } = useContext(ChallengesContext);
+
+    const percontToNextLevel = Math.round(currentExperience * 100) / experienceToNextLevel;
+
     return (
         <header className={styles.experienceBar}>
             <span>0 xp</span>
             <div>
-                <div style={{ width: '50%' }} />
-                <span className={styles.currentExperience} style={{ left: '50%' }}>300 xp</span>
+                <div style={{ width: `${percontToNextLevel}%` }} />
+                <span className={styles.currentExperience} style={{ left: `${percontToNextLevel}%` }}>{currentExperience} xp</span>
             </div>
-            <span>600 xp</span>
+            <span>{experienceToNextLevel} xp</span>
         </header>
 
     );
